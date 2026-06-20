cbb8f4dfdecdca08eb61f9b9b0eb26e6be150e4e
KezelHugo <kevin.bsl.2001@hotmail.com>
2026-06-20 16:02:35 -0500

    chore: move firebase credentials to environment variables

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,15 +5,15 @@ import { getFirestore } from 'firebase/firestore';
 import { Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Reemplaza estas credenciales con las de tu proyecto en la consola de Firebase
+// Carga las credenciales usando variables de entorno nativas de Expo
 const firebaseConfig = {
-  apiKey: "AIzaSyCpowU-f9Z_1RpO9AQRF8eQ0zYyZ7IczIU",
-  authDomain: "barberapp-d1efb.firebaseapp.com",
-  projectId: "barberapp-d1efb",
-  storageBucket: "barberapp-d1efb.firebasestorage.app",
-  messagingSenderId: "341672335598",
-  appId: "1:341672335598:web:ec0d5adef4b649c8673bff",
-  measurementId: "G-4J74G6H4FL"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializa la app de Firebase previniendo duplicaciones durante el hot reload
